b6bb7a6bad82312ff5a4570a1b16e9a98c8d8a40
patrice cadiot <pathibul.r@gmail.com>
2021-07-14 21:28:42 +0200

    works for number from 1 to 99

--- roman_numerals.rb
+++ roman_numerals.rb
@@ -1,28 +1,37 @@
 class Integer
-    def to_roman
-        case self
-        when 1 
-            'I'
-        when 2
-            'II'
-        when 3
-            'III'
-        when 4
-            'IV'
-        when 5
-            'V'
-        when 6
-            'VI'
-        when 7
-            'VII'
-        when 8
-            'VIII'
-        when 9
-            'IX'
-        end
-
-        if self.to_s.size > 2 
-
-        end
+  def to_roman
+    numbers = self.digits
+    rank = numbers.size
+    case rank
+    when 1
+      units(numbers.first, 'I', 'V', 'X')
+    when 2
+      units(numbers[1], 'X', 'L', 'C') + units(numbers[0], 'I', 'V', 'X')
     end
+  end
+
+  def units(number, unit, half, full)
+    case number
+    when 1
+      unit
+    when 2
+      unit * 2
+    when 3
+      unit * 3
+    when 4
+      unit + half
+    when 5
+      half
+    when 6
+      half + unit
+    when 7
+      half + unit * 2
+    when 8
+      half + unit * 3
+    when 9
+      unit + full
+    end
+  end
 end
+
+27.to_roman
